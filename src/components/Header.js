5aa40eabd8b4102ff8e0d469f654b1a485dd21a9
Piyush Pandey <piyushpvns@gmail.com>
2024-02-23 01:43:39 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -75,7 +75,7 @@ const Header = () => {
             onClick={handleGPTSearchClick}
             className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mx-5"
           >
-            SearchGPT
+            {showGPTSearch ? "Home" : "SeachGPT"}
           </button>
 
           {user.photoURL === null ? (
